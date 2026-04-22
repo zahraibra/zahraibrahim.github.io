6debdb3e0bd9a8cf6069102a41a793369be36689
ZAHRA IBRAHIM <zahraibra346@gmail.com>
2026-04-22 17:20:43 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const observerOptions = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.reveal').forEach((el) => {
+    observer.observe(el);
+});
